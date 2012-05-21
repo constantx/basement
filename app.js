e0af6253f1e89f54561c3cf9031ee2b51de86f2c
Truong Nguyen <constantx@gmail.com>
2012-05-21 12:58:56 -0400

    trying to fix socket

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 3000;
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
-    app.set('view options', { layout: false });
+    app.set('view options', { layout: false, pretty: true });
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
